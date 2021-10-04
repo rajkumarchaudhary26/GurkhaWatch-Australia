537731f127b0580a72205e7c2653c7126dc0f50b
Sugam Baskota <sugam.baskota@pontoonads.com>
2021-10-04 11:28:06 +0545

    Issue #5 initial changes for server
    * Added gunicorn package
    * Added static images

--- GurkhaWatch/jstools/tailwind.config.js
+++ GurkhaWatch/jstools/tailwind.config.js
@@ -5,14 +5,31 @@ module.exports = {
   },
   purge: {
     enabled: false, //true for production build
-    content: [
-      '../**/templates/*.html',
-      '../**/templates/**/*.html'
-    ]
+    content: ["../**/templates/*.html", "../**/templates/**/*.html"],
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#222221",
+        primary2: "#242424",
+        primary3: "#1e1e1e",
+        primary4: "#202020",
+        primary5: "#2f2f2f",
+        primary6: "#1f1f1f",
+        primary7: "#151516",
+        secondary: "#dededf",
+        secondary2: "#7a7a7a",
+        secondary3: "#a5a5a6",
+        golden: "#ab6e36",
+      },
+      fontFamily: {
+        jura: ['"Jura"', "sans-serif"],
+      },
+      rotate: {
+        "30": "30deg"
+      }
+    },
   },
   variants: {},
   plugins: [],
-}
+};
