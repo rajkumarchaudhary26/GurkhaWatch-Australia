cd4b8d9f3aca3be8fa3ff4e4444385608060d96b
Sugam Baskota <sugam.baskota@pontoonads.com>
2021-10-04 12:07:11 +0545

    Issue #5 Initial changes for server
    * Added test server's ip to ALLOWED_HOSTS
    * Tailwind configured to purge

--- GurkhaWatch/jstools/tailwind.config.js
+++ GurkhaWatch/jstools/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: {
-    enabled: false, //true for production build
+    enabled: true, //true for production build
     content: ["../**/templates/*.html", "../**/templates/**/*.html"],
   },
   theme: {
