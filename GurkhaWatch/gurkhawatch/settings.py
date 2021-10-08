b337f8f5d82d1c281b2769aa4cb47cbf3397ae3b
Sugam Baskota <sugam.baskota@pontoonads.com>
2021-10-08 11:49:21 +0545

    Issue #5 initial changes for server
    * Added subdomain to ALLOWED_HOSTS

--- GurkhaWatch/gurkhawatch/settings.py
+++ GurkhaWatch/gurkhawatch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-+t!qf80ufiyy!gw37acfr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '88.198.106.179']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gurkhawatch.pontoonads.com', 'gurkhawatch.com.au']
 
 
 # Application definition
